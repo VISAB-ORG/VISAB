91673f3800689f1052478aefbefcf8b588371de4
VocalTrance <platenpaypal@gmail.com>
2021-05-09 16:07:55 +0200

    Add RepositoryBase that can be extended by different repositories

---
+++ src/main/java/org/visab/repository/RepositoryBase.java
@@ -0,0 +1,105 @@
+package org.visab.repository;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class RepositoryBase {
+
+    protected String baseDirectory;
+
+    public RepositoryBase(String baseDirectory) {
+        this.baseDirectory = baseDirectory;
+
+        // Create the base directory if it doesn't exist.
+        new File(baseDirectory).mkdirs();
+    }
+
+    /**
+     * Reads the contents of a file at a given path
+     *
+     * @param filePath The path to the file
+     * @return The contents of the file or empty string if unsuccessful
+     */
+    public String readFileContents(String filePath) {
+        try {
+            return new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return "";
+        }
+    }
+
+    /**
+     * Reads in a file at a given path
+     * 
+     * @param filePath The path to the file
+     * @return The read in file
+     */
+    public File loadFile(String filePath) {
+        return new File(filePath);
+    }
+
+    /**
+     * Writes string content to a file at a given path
+     *
+     * @param filePath The path to the file
+     * @param content  The content of the file
+     * @return True if file was successfully written, false else
+     */
+    public boolean writeToFile(String filePath, String content) {
+        var file = new File(filePath);
+
+        try (var writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Deletes a file at a given path
+     *
+     * @param filePath The path of the file to delete
+     * @return True if deleted, false else
+     */
+    public boolean deleteFile(String filePath) {
+        var file = new File(filePath);
+
+        if (!file.isDirectory())
+            return file.delete();
+        else
+            return deleteFolder(file);
+    }
+
+    /**
+     * Deletes a folder and all its contents
+     * 
+     * @param folder The folder to delete
+     * @return True if fully deleted, false else
+     */
+    public boolean deleteFolder(File folder) {
+        var files = folder.listFiles();
+        if (files != null) {
+            for (File f : files) {
+                if (f.isDirectory())
+                    deleteFolder(f);
+                else {
+                    if (!f.delete())
+                        return false;
+                }
+            }
+        }
+
+        return folder.delete();
+    }
+
+}
--- src/main/java/org/visab/repository/VISABRepository.java
+++ src/main/java/org/visab/repository/VISABRepository.java
@@ -17,60 +17,16 @@ import org.visab.util.Settings;
  * @author moritz
  *
  */
-public class VISABRepository {
-
-    private static final String baseDir = Settings.DATA_PATH;
+public class VISABRepository extends RepositoryBase {
 
     public VISABRepository() {
-        // Create the base directory
-        new File(baseDir).mkdirs();
+        super(Settings.DATA_PATH);
     }
 
-    /**
-     * Reads the contents of a file at a given path
-     *
-     * @param filePath The path to the file
-     * @return The contents of the file or empty string if unsuccessful
-     */
-    public String readFile(String filePath) {
-        try {
-            return new String(Files.readAllBytes(Paths.get(filePath)));
-        } catch (IOException e) {
-            e.printStackTrace();
-
-            return "";
-        }
-    }
-
-    /**
-     * Writes string content to a file at a given path
-     *
-     * @param filePath The path to the file
-     * @param content  The content of the file
-     * @return True if file was successfully written, false else
-     */
-    public boolean writeFile(String filePath, String content) {
-        var file = new File(filePath);
-
-        try (var writer = new BufferedWriter(new FileWriter(file))) {
-            writer.write(content);
-        } catch (IOException e) {
-            e.printStackTrace();
-
-            return false;
-        }
-
-        return true;
-    }
-
-    public File loadFile(String game, String fileName) {
-        var filePath = baseDir + game + "/" + fileName;
+    public File loadFile(String fileName, String game) {
+        var filePath = baseDirectory + game + "/" + fileName;
 
         return loadFile(filePath);
-    }
-
-    public File loadFile(String filePath) {
-        return new File(filePath);
     }
 
     /**
@@ -80,55 +36,24 @@ public class VISABRepository {
      * @param fileName The name of the file
      * @return True if deleted, false else
      */
-    public boolean deleteFileByName(String game, String fileName) {
-        var filePath = baseDir + game + "/" + fileName;
+    public boolean deleteFileByName(String fileName, String game) {
+        var filePath = baseDirectory + game + "/" + fileName;
 
-        return deleteFileByPath(filePath);
+        return deleteFile(filePath);
     }
 
     /**
-     * Deletes a file at a given path
-     *
-     * @param filePath The path of the file to delete
-     * @return True if deleted, false else
-     */
-    public boolean deleteFileByPath(String filePath) {
-        var file = new File(filePath);
-
-        if (!file.isDirectory())
-            return file.delete();
-        else
-            return deleteFolder(file);
-    }
-
-    private boolean deleteFolder(File folder) {
-        var files = folder.listFiles();
-        if (files != null) {
-            for (File f : files) {
-                if (f.isDirectory())
-                    deleteFolder(f);
-                else {
-                    if (!f.delete())
-                        return false;
-                }
-            }
-        }
-
-        return folder.delete();
-    }
-
-    /**
-     * Loads a file from the database
+     * Loads a VISABFile file from the repository
      *
      * @param <T>      The type of the file
      * @param game     The game of the file
      * @param fileName The name of the file
      * @return
      */
-    public <T extends IVISABFile> T loadFileByName(String game, String fileName) {
-        var filePath = baseDir + game + "/" + fileName;
+    public <T extends IVISABFile> T loadVISABFile(String fileName, String game) {
+        var filePath = baseDirectory + game + "/" + fileName;
 
-        return loadFileByPath(game, filePath);
+        return this.<T>loadVISABFileByPath(game, filePath);
     }
 
     /**
@@ -140,22 +65,28 @@ public class VISABRepository {
      * @return
      */
     @SuppressWarnings("unchecked")
-    public <T extends IVISABFile> T loadFileByPath(String game, String filePath) {
-        var json = readFile(filePath);
+    public <T extends IVISABFile> T loadVISABFileByPath(String filePath, String game) {
+        var json = readFileContents(filePath);
 
         var file = AssignByGame.getDeserializedFile(json, game);
 
         return file != null ? (T) file : null;
     }
 
+    /**
+     * Loads a VISABFileBase object from a given path
+     * 
+     * @param filePath The path to the file
+     * @return The VISABFileBase
+     */
     public VISABFileBase loadBaseFile(String filePath) {
-        var json = readFile(filePath);
+        var json = readFileContents(filePath);
 
         return JsonConvert.deserializeJson(json, VISABFileBase.class);
     }
 
     /**
-     * Saves a file to the database
+     * Saves a file to the repository
      *
      * @param visabFile The file to save
      * @param fileName  The name of the file
@@ -164,7 +95,7 @@ public class VISABRepository {
     public boolean saveFile(IVISABFile visabFile, String fileName) {
         var json = JsonConvert.serializeObject(visabFile);
 
-        var fileDir = baseDir + visabFile.getGame();
+        var fileDir = baseDirectory + visabFile.getGame();
         new File(fileDir).mkdirs();
 
         var filePath = fileDir + "/" + fileName;
@@ -172,7 +103,7 @@ public class VISABRepository {
         if (!filePath.contains("."))
             filePath += ".visab2";
 
-        return writeFile(filePath, json);
+        return writeToFile(filePath, json);
     }
 
 }
