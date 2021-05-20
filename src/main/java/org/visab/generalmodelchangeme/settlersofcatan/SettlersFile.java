e3a5721392a0c7449a88a8781913bd5fec7c3555
VocalTrance <platenpaypal@gmail.com>
2021-05-20 15:19:52 +0200

    Readd settlers file

---
+++ src/main/java/org/visab/generalmodelchangeme/settlersofcatan/SettlersFile.java
@@ -0,0 +1,11 @@
+package org.visab.generalmodelchangeme.settlersofcatan;
+
+import org.visab.generalmodelchangeme.VISABFileBase;
+import org.visab.util.AssignByGame;
+
+public class SettlersFile extends VISABFileBase {
+
+    public SettlersFile() {
+        super(AssignByGame.SETTLERS_OF_CATAN_STRING, "2.0");
+    }
+}
