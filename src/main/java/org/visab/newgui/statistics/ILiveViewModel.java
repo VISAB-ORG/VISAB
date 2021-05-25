c946a0e8bbac6128293ecdfe8aefa5f8d1977b16
VocalTrance <platenpaypal@gmail.com>
2021-05-25 18:45:23 +0200

    Add LiveStatisticsViewModelBase, modify ILiveViewModel, so that it can be loaded without explicitly given the generic type.

--- src/main/java/org/visab/newgui/statistics/ILiveViewModel.java
+++ src/main/java/org/visab/newgui/statistics/ILiveViewModel.java
@@ -1,8 +1,11 @@
 package org.visab.newgui.statistics;
 
 import org.visab.globalmodel.IStatistics;
+import org.visab.processing.ILiveViewable;
 
 public interface ILiveViewModel<TStatistics extends IStatistics> {
+
+    void initialize(ILiveViewable<? extends IStatistics> listener);
 
     void notifyStatisticsAdded(TStatistics newStatistics);
 
