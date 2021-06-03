cceb20d0053bedcaaf671ad3e47f81b12691181d
Leon Römer <roemerl@uni-hildesheim.de>
2021-06-03 17:09:07 +0200

    Created initial versions of necessary classes to receive data from Unity-Settlers.

---
+++ src/main/java/org/visab/globalmodel/settlers/SettlersStatistics.java
@@ -0,0 +1,25 @@
+package org.visab.globalmodel.settlers;
+
+import org.visab.globalmodel.IStatistics;
+
+/**
+ * The SettlersStatistics class, representing the information given by the
+ * Unity Settlers of Catan Game.
+ *
+ * @author leonr
+ *
+ */
+public class SettlersStatistics implements IStatistics {
+	
+	// TODO: Evaluate attributes to compute here
+	
+	private String game;
+
+	@Override
+	public String getGame() {
+		// TODO Auto-generated method stub
+		return game;
+	}
+
+    
+}
