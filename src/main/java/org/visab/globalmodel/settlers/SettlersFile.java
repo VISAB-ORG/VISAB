5cf3dc34875a70a4fdb43864209ba28723f2ce94
Leon Römer <roemerl@uni-hildesheim.de>
2021-06-03 17:09:07 +0200

    Created initial versions of necessary classes to receive data from Unity-Settlers.

---
+++ src/main/java/org/visab/globalmodel/settlers/SettlersFile.java
@@ -0,0 +1,28 @@
+package org.visab.globalmodel.settlers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.visab.globalmodel.BasicVISABFile;
+import org.visab.util.AssignByGame;
+
+/**
+ * This class represents the structure of a VISAB data file
+ * generated from Settlers of Catan data.
+ * 
+ * @author leonr
+ *
+ */
+public class SettlersFile extends BasicVISABFile {
+
+    private List<SettlersStatistics> statistics = new ArrayList<>();
+
+    public SettlersFile() {
+        super(AssignByGame.SETTLERS_OF_CATAN_STRING, "2.0");
+    }
+
+    public List<SettlersStatistics> getStatistics() {
+        return statistics;
+    }
+
+}
---
+++ src/main/java/org/visab/globalmodel/settlers/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains all globally used models that are settlers-specific.
+ * <p>
+ * Every model class that is only used for settlers-specific purposes shall be
+ * placed in this package. Generic models for VISAB or specific models for other
+ * games should not be placed in here.
+ * </p>
+ */
+package org.visab.globalmodel.settlers;
