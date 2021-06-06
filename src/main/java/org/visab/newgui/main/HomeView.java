a9d01214fa3307c9025f6b4514a834d76c7c44c5
vanessas97 <vanessa.schriefer@gmx.de>
2021-06-06 22:02:03 +0200

    MVVM for Home Window.

---
+++ src/main/java/org/visab/newgui/main/HomeView.java
@@ -0,0 +1,17 @@
+package org.visab.newgui.main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import de.saxsys.mvvmfx.FxmlView;
+import javafx.fxml.Initializable;
+
+public class HomeView implements FxmlView<HomeViewModel>, Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
---
+++ src/main/java/org/visab/newgui/main/HomeViewModel.java
@@ -0,0 +1,7 @@
+package org.visab.newgui.main;
+
+import de.saxsys.mvvmfx.ViewModel;
+
+public class HomeViewModel implements ViewModel {
+    
+}
