558d190af7ff092efc1353f76c5e01fddc23f805
vanessas97 <vanessa.schriefer@gmx.de>
2021-06-07 20:12:57 +0200

    Open API Dashboard Window via Menu Item.

--- src/main/java/org/visab/newgui/main/HomeViewModel.java
+++ src/main/java/org/visab/newgui/main/HomeViewModel.java
@@ -1,7 +1,31 @@
 package org.visab.newgui.main;
 
-import de.saxsys.mvvmfx.ViewModel;
+import org.visab.newgui.AppMain;
+import org.visab.newgui.DynamicViewLoader;
+import org.visab.newgui.ViewModelBase;
+import org.visab.newgui.webapi.WebApiView;
 
-public class HomeViewModel implements ViewModel {
-    
+import de.saxsys.mvvmfx.FluentViewLoader;
+import de.saxsys.mvvmfx.ViewModel;
+import de.saxsys.mvvmfx.utils.commands.Command;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HomeViewModel extends ViewModelBase implements ViewModel {
+
+	private Command openApiDashboard;
+
+	public Command openApi() {
+		if (openApiDashboard == null) {
+			openApiDashboard = runnableCommand(() -> {
+				DynamicViewLoader.showView(WebApiView.class, "API Dashboard");
+
+			});
+		}
+
+		return openApiDashboard;
+
+	}
+
 }
