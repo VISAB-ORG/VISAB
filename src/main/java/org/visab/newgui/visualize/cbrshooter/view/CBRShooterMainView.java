6424bcee02374db84804b4be29f9d3325f0d05a3
VocalTrance <platenpaypal@gmail.com>
2021-06-15 17:03:18 +0200

    Add Ivisualizeview

--- src/main/java/org/visab/newgui/visualize/cbrshooter/view/CBRShooterMainView.java
+++ src/main/java/org/visab/newgui/visualize/cbrshooter/view/CBRShooterMainView.java
@@ -1,8 +1,24 @@
 package org.visab.newgui.visualize.cbrshooter.view;
 
+import java.util.List;
+
+import org.visab.newgui.visualize.IVisualizeView;
+import org.visab.newgui.visualize.cbrshooter.viewmodel.CBRShooterStatisticsViewModel;
+
+import de.saxsys.mvvmfx.InjectViewModel;
+import de.saxsys.mvvmfx.ViewModel;
+
 /**
  * View that has the tabcontrol containing all possible views
  */
-public class CBRShooterMainView {
-    
+public class CBRShooterMainView implements IVisualizeView {
+
+    @InjectViewModel
+    CBRShooterStatisticsViewModel statisticsViewModel;
+
+    @Override
+    public List<ViewModel> getTabViewModels() {
+        return List.of(statisticsViewModel);
+    }
+
 }
