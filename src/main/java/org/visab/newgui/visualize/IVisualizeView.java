8983fbd636511753834cd155b78b5a8c29fe1fc3
VocalTrance <platenpaypal@gmail.com>
2021-06-15 17:03:18 +0200

    Add Ivisualizeview

---
+++ src/main/java/org/visab/newgui/visualize/IVisualizeView.java
@@ -0,0 +1,11 @@
+package org.visab.newgui.visualize;
+
+import java.util.List;
+
+import de.saxsys.mvvmfx.ViewModel;
+
+public interface IVisualizeView {
+
+    List<ViewModel> getTabViewModels();
+
+}
