f805294142985441abaff1aeae77148f37765ff6
leRoe93 <leon.roemer@icloud.com>
2021-07-01 13:46:02 +0200

    Reorganized replay view as a new template and various other changes.

--- src/main/java/org/visab/newgui/visualize/cbrshooter/viewmodel/CBRShooterReplayViewModel.java
+++ src/main/java/org/visab/newgui/visualize/cbrshooter/viewmodel/CBRShooterReplayViewModel.java
@@ -1,5 +1,20 @@
 package org.visab.newgui.visualize.cbrshooter.viewmodel;
 
-public class CBRShooterReplayViewModel {
-    
+import org.visab.newgui.ViewModelBase;
+import org.visab.newgui.visualize.VisualizeScope;
+
+import de.saxsys.mvvmfx.InjectScope;
+
+public class CBRShooterReplayViewModel extends ViewModelBase {
+
+    @InjectScope
+    VisualizeScope scope;
+
+    /**
+     * Called after the instance was constructed by javafx/mvvmfx.
+     */
+    public void initialize() {
+        // TODO: Add necessary logic
+    }
+
 }
