eba8450c3ec0d249efdaa2c8609c9a37377eb1a8
leRoe93 <leon.roemer@icloud.com>
2021-07-14 19:56:04 +0200

    Added a class to handle the provision of visuals for a player.

---
+++ src/main/java/org/visab/newgui/visualize/cbrshooter/model/PlayerVisuals.java
@@ -0,0 +1,61 @@
+package org.visab.newgui.visualize.cbrshooter.model;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+
+/**
+ * This class is responsible for providing the visuals for a specific player.
+ * 
+ * @author leonr
+ *
+ */
+public class PlayerVisuals {
+
+    private ImageView playerIcon;
+
+    private ImageView playerDeath;
+
+    private ImageView playerPlanChange;
+
+    private Color playerColor;
+
+    public PlayerVisuals(ImageView playerIcon, ImageView playerDeath, ImageView playerPlanChange, Color playerColor) {
+        this.playerIcon = playerIcon;
+        this.playerDeath = playerDeath;
+        this.playerPlanChange = playerPlanChange;
+        this.playerColor = playerColor;
+    }
+
+    public ImageView getPlayerIcon() {
+        return playerIcon;
+    }
+
+    public void setPlayerIcon(ImageView playerIcon) {
+        this.playerIcon = playerIcon;
+    }
+
+    public ImageView getPlayerDeath() {
+        return playerDeath;
+    }
+
+    public void setPlayerDeath(ImageView playerDeath) {
+        this.playerDeath = playerDeath;
+    }
+
+    public ImageView getPlayerPlanChange() {
+        return playerPlanChange;
+    }
+
+    public void setPlayerPlanChange(ImageView playerPlanChange) {
+        this.playerPlanChange = playerPlanChange;
+    }
+
+    public Color getPlayerColor() {
+        return playerColor;
+    }
+
+    public void setPlayerColor(Color playerColor) {
+        this.playerColor = playerColor;
+    }
+
+}
