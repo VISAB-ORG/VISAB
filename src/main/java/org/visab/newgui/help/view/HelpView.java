0474484b67dafc34f925ea4ed7aba4908e7ce459
vanessas97 <vanessa.schriefer@gmx.de>
2021-07-27 13:30:58 +0200

    Various exploratory changes to display a PDF within the JavaFX application.

--- src/main/java/org/visab/newgui/help/view/HelpView.java
+++ src/main/java/org/visab/newgui/help/view/HelpView.java
@@ -1,28 +1,44 @@
 package org.visab.newgui.help.view;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import org.visab.newgui.help.viewmodel.HelpViewModel;
+import org.visab.oldgui.GUIMain;
+import org.visab.workspace.config.ConfigManager;
+
+import com.dansoftware.pdfdisplayer.PDFDisplayer;
 
 import de.saxsys.mvvmfx.FxmlView;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
-/*
-import javafx.scene.web.WebEngine;
+import javafx.scene.layout.Pane;
 import javafx.scene.web.WebView;
-*/
 public class HelpView implements FxmlView<HelpViewModel>, Initializable{
 	
 	@FXML
-    //private WebView showPDF;
+    private WebView showPDF;
 
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		//showPDF.getEngine().load("http://google.com");
-	}
+		//showPDF.getEngine().load("http://www.google.com");
+		
+		String url = getClass().getResource("/pdf/visab_documentation.html").toExternalForm();
+		System.out.println(url);
+		
+		File file = new File(url);
 
+		showPDF.getEngine().load(file.toURI().toString());
+
+		// Would work for our online hosted documentation
+        // showPDF.getEngine().load("https://visab-org.github.io");
+        
+
+
+	}
 }
