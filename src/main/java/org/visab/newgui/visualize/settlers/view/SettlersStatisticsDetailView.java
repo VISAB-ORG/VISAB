03c5a9f554a3c7b798893cb29469fbfc0dae03b4
TimKuno <tim.kunold@gmx.de>
2021-08-02 20:43:55 +0200

    fixes statisticsDetailView loading

--- src/main/java/org/visab/newgui/visualize/settlers/view/SettlersStatisticsDetailView.java
+++ src/main/java/org/visab/newgui/visualize/settlers/view/SettlersStatisticsDetailView.java
@@ -1,23 +1,55 @@
 package org.visab.newgui.visualize.settlers.view;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import org.visab.newgui.visualize.ComparisonRowBase;
 import org.visab.newgui.visualize.settlers.viewmodel.SettlersStatisticsViewModel;
 
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.beans.value.ObservableValue;
 
 public class SettlersStatisticsDetailView implements FxmlView<SettlersStatisticsViewModel>, Initializable {
-    
+
+    @FXML
+    TableView<ComparisonRowBase<?>> detailStatistics;
+
+    @FXML
+    LineChart<Integer, Number> detailedPlayerStatistics;
+
     @InjectViewModel
     SettlersStatisticsViewModel viewModel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
-        
+        detailStatistics.setItems(viewModel.getDetailedStatistics());
+        addComparisonColumns();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void addComparisonColumns() {
+        var columns = new ArrayList<TableColumn<ComparisonRowBase<?>, ?>>();
+
+        var playerNames = viewModel.getPlayerNames();
+        for (int i = 0; i < playerNames.size(); i++) {
+            var name = playerNames.get(i);
+            var type = viewModel.getPlayerInformation().get(name);
+            var column = new TableColumn<ComparisonRowBase<?>, Object>(name + " (" + type + ")");
+
+            // Create cell value factory
+            column.setCellValueFactory(
+                    cellData -> (ObservableValue<Object>) cellData.getValue().getPlayerValues().get(name));
+            columns.add(column);
+        }
+
+        detailStatistics.getColumns().addAll(columns);
     }
 
 }
