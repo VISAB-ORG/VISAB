cb5991bd77cc132f5d7d782bf53de395f1d18208
VocalTrance <platenpaypal@gmail.com>
2021-08-03 17:41:12 +0200

    Remove oldgui

--- src/main/java/org/visab/oldgui/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This package contains the controllers of the legacy VISAB GUI.
- * <p>
- * In the future this package will get merged / substituted with contents of the
- * "newgui" package because the legacy implementation does not really follow an
- * abstracted design pattern to distinct between models, views and controls.
- * </p>
- */
-package org.visab.oldgui;
