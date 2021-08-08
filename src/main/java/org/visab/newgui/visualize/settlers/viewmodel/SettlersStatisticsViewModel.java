816691c5ea711b5dba635baf9d24fb6908fe9f01
TimKuno <tim.kunold@gmx.de>
2021-08-08 14:24:43 +0200

    removed unused imports

--- src/main/java/org/visab/newgui/visualize/settlers/viewmodel/SettlersStatisticsViewModel.java
+++ src/main/java/org/visab/newgui/visualize/settlers/viewmodel/SettlersStatisticsViewModel.java
@@ -22,10 +22,8 @@ import org.visab.newgui.visualize.settlers.view.SettlersStatisticsDetailView;
 
 import de.saxsys.mvvmfx.InjectScope;
 import de.saxsys.mvvmfx.utils.commands.Command;
-import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
