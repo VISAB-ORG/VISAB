17e39810e4e8ddb94bf6d5203b9d17571fc35ffb
leRoe93 <leon.roemer@icloud.com>
2021-08-08 19:21:32 +0200

    Add roads, villages and cities as imnages to the Player class.

--- src/main/java/org/visab/newgui/visualize/settlers/model/Player.java
+++ src/main/java/org/visab/newgui/visualize/settlers/model/Player.java
@@ -1,17 +1,18 @@
 package org.visab.newgui.visualize.settlers.model;
 
+import java.util.List;
+
 import org.visab.globalmodel.Vector2;
 import org.visab.globalmodel.settlers.PlayerResources;
 
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.ListProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.ObservableList;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
 /**
@@ -25,16 +26,16 @@ public class Player {
     private String name;
 
     // Table + Replay view
-    private IntegerProperty villageCountPropery = new SimpleIntegerProperty();
-    private ListProperty<Vector2> villagePositionsProperty = new SimpleListProperty<Vector2>();
-    private IntegerProperty streetCountPropery = new SimpleIntegerProperty();
-    private ListProperty<Vector2> streetPositionsProperty = new SimpleListProperty<Vector2>();
+    private IntegerProperty villageCountProperty = new SimpleIntegerProperty();
+    private ObjectProperty<List<Vector2>> villagePositionsProperty = new SimpleObjectProperty<List<Vector2>>();
+    private IntegerProperty streetCountProperty = new SimpleIntegerProperty();
+    private ObjectProperty<List<Vector2>> streetPositionsProperty = new SimpleObjectProperty<List<Vector2>>();
     private IntegerProperty cityCountProperty = new SimpleIntegerProperty();
-    private ListProperty<Vector2> cityPositionsProperty = new SimpleListProperty<Vector2>();
+    private ObjectProperty<List<Vector2>> cityPositionsProperty = new SimpleObjectProperty<List<Vector2>>();
     private ObjectProperty<PlayerResources> resourcesProperty = new SimpleObjectProperty<PlayerResources>();
     private BooleanProperty hasLongestRoadProperty = new SimpleBooleanProperty();
     private IntegerProperty longestRoadProperty = new SimpleIntegerProperty();
-    private ListProperty<String> planActionsProperty = new SimpleListProperty<String>();
+    private ObjectProperty<List<String>> planActionsProperty = new SimpleObjectProperty<List<String>>();
     private IntegerProperty victoryPointsProperty = new SimpleIntegerProperty();
     private ObjectProperty<PlayerResources> resourcesGainedProperty = new SimpleObjectProperty<PlayerResources>();
     private BooleanProperty isMyTurnProperty = new SimpleBooleanProperty();
@@ -43,17 +44,24 @@ public class Player {
 
     // Visual table
     private BooleanProperty showPlayerProperty = new SimpleBooleanProperty(true);
-    private BooleanProperty showRoadsPropery = new SimpleBooleanProperty(true);
-    private BooleanProperty showVillagesPropery = new SimpleBooleanProperty(true);
-    private BooleanProperty showCitiesPropery = new SimpleBooleanProperty(true);
+    private BooleanProperty showRoadProperty = new SimpleBooleanProperty(true);
+    private BooleanProperty showVillagesProperty = new SimpleBooleanProperty(true);
+    private BooleanProperty showCitiesProperty = new SimpleBooleanProperty(true);
     private BooleanProperty showPlanChangeProperty = new SimpleBooleanProperty(true);
+
+    private Image playerVillage;
+    private Image playerCity;
+    private Image playerRoad;
 
     public Player(String name) {
         this.name = name;
     }
 
-    public void initializeVisuals(Color playerColor) {
+    public void initializeVisuals(Color playerColor, Image playerRoad, Image playerVillage, Image playerCity) {
         this.playerColorProperty.set(playerColor);
+        this.playerRoad = playerRoad;
+        this.playerVillage = playerVillage;
+        this.playerCity = playerCity;
     }
 
     /**
@@ -65,16 +73,16 @@ public class Player {
      */
     public void updatePlayerData(org.visab.globalmodel.settlers.Player playerInfo) {
 
-        villageCountPropery.set(playerInfo.getVillageCount());
-        villagePositionsProperty.set((ObservableList<Vector2>) playerInfo.getVillagePositions());
-        streetCountPropery.set(playerInfo.getStreetCount());
-        streetPositionsProperty.set((ObservableList<Vector2>) playerInfo.getStreetPositions());
+        villageCountProperty.set(playerInfo.getVillageCount());
+        villagePositionsProperty.set(playerInfo.getVillagePositions());
+        streetCountProperty.set(playerInfo.getStreetCount());
+        streetPositionsProperty.set(playerInfo.getStreetPositions());
         cityCountProperty.set(playerInfo.getCityCount());
-        cityPositionsProperty.set((ObservableList<Vector2>) playerInfo.getCityPositions());
+        cityPositionsProperty.set(playerInfo.getCityPositions());
         resourcesProperty.set(playerInfo.getResources());
         hasLongestRoadProperty.set(playerInfo.isHasLongestRoad());
         longestRoadProperty.set(playerInfo.getLongestRoad());
-        planActionsProperty.set((ObservableList<String>) playerInfo.getPlanActions());
+        planActionsProperty.set(playerInfo.getPlanActions());
         victoryPointsProperty.set(playerInfo.getVictoryPoints());
         resourcesGainedProperty.set(playerInfo.getResourcesGained());
         isMyTurnProperty.set(playerInfo.isMyTurn());
@@ -85,35 +93,35 @@ public class Player {
         return name;
     }
 
-    public IntegerProperty villageCountPropery() {
-        return villageCountPropery;
+    public IntegerProperty villageCountProperty() {
+        return villageCountProperty;
     }
 
-    public ListProperty<Vector2> villagePositionsProperty() {
+    public ObjectProperty<List<Vector2>> villagePositionsProperty() {
         return villagePositionsProperty;
     }
 
-    public IntegerProperty streetCountPropery() {
-        return streetCountPropery;
+    public IntegerProperty streetCountProperty() {
+        return streetCountProperty;
     }
 
-    public ListProperty<Vector2> streetPositionsProperty() {
+    public ObjectProperty<List<Vector2>> streetPositionsProperty() {
         return streetPositionsProperty;
     }
 
-    public BooleanProperty showVillagesPropery() {
-        return showVillagesPropery;
+    public BooleanProperty showVillagesProperty() {
+        return showVillagesProperty;
     }
 
-    public BooleanProperty showCitiesPropery() {
-        return showCitiesPropery;
+    public BooleanProperty showCitiesProperty() {
+        return showCitiesProperty;
     }
 
     public IntegerProperty cityCountProperty() {
         return cityCountProperty;
     }
 
-    public ListProperty<Vector2> cityPositionsProperty() {
+    public ObjectProperty<List<Vector2>> cityPositionsProperty() {
         return cityPositionsProperty;
     }
 
@@ -129,7 +137,7 @@ public class Player {
         return longestRoadProperty;
     }
 
-    public ListProperty<String> planActionsProperty() {
+    public ObjectProperty<List<String>> planActionsProperty() {
         return planActionsProperty;
     }
 
@@ -157,12 +165,24 @@ public class Player {
         return showPlayerProperty;
     }
 
-    public BooleanProperty showRoadsPropery() {
-        return showRoadsPropery;
+    public BooleanProperty showRoadProperty() {
+        return showRoadProperty;
     }
 
     public BooleanProperty showPlanChangeProperty() {
         return showPlanChangeProperty;
+    }
+
+    public Image getPlayerVillage() {
+        return playerVillage;
+    }
+
+    public Image getPlayerCity() {
+        return playerCity;
+    }
+
+    public Image getPlayerRoad() {
+        return playerRoad;
     }
 
 }
