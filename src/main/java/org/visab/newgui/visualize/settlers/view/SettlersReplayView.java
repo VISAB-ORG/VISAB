131633a4eb9624f0b20e33690c4ef4be19d0a433
leRoe93 <leon.roemer@icloud.com>
2021-08-08 19:23:01 +0200

    Initial map-less implementation analogous to CBRShooter replay view.

--- src/main/java/org/visab/newgui/visualize/settlers/view/SettlersReplayView.java
+++ src/main/java/org/visab/newgui/visualize/settlers/view/SettlersReplayView.java
@@ -1,9 +1,226 @@
 package org.visab.newgui.visualize.settlers.view;
 
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.visab.globalmodel.Vector2;
+import org.visab.globalmodel.settlers.SettlersStatistics;
+import org.visab.newgui.ResourceHelper;
+import org.visab.newgui.UiHelper;
+import org.visab.newgui.visualize.cbrshooter.model.CoordinateHelper;
+import org.visab.newgui.visualize.settlers.model.Player;
+import org.visab.newgui.visualize.settlers.model.PlayerVisualsRow;
 import org.visab.newgui.visualize.settlers.viewmodel.SettlersReplayViewModel;
 
 import de.saxsys.mvvmfx.FxmlView;
+import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 
-public class SettlersReplayView implements FxmlView<SettlersReplayViewModel> {
-    
+public class SettlersReplayView implements FxmlView<SettlersReplayViewModel>, Initializable {
+
+    @FXML
+    private Slider frameSlider;
+    @FXML
+    private Slider veloSlider;
+
+    @FXML
+    private Pane drawPane;
+
+    @FXML
+    private ToggleButton playPauseButton;
+
+    @FXML
+    private Label turnValueLabel;
+
+    @FXML
+    private Label turnTimeStampValueLabel;
+
+    @FXML
+    private Label diceRollsValueLabel;
+
+    @FXML
+    private TableView<org.visab.newgui.visualize.settlers.model.Player> playerDataTable;
+    @FXML
+    private TableView<PlayerVisualsRow> playerVisualsTable;
+
+    private static final double DRAW_PANE_WIDTH = 550.0;
+
+    private static final Vector2 STANDARD_ICON_VECTOR = new Vector2(16, 16);
+
+    private CoordinateHelper coordinateHelper;
+
+    private Image pauseImage = new Image(ResourceHelper.IMAGE_PATH + "pause.png");
+    private Image playImage = new Image(ResourceHelper.IMAGE_PATH + "play.png");
+
+    private ImageView playImageView = UiHelper.resizeImage(new ImageView(playImage), new Vector2(32, 32));
+    private ImageView pauseImageView = UiHelper.resizeImage(new ImageView(pauseImage), new Vector2(32, 32));
+
+    private ObjectProperty<SettlersStatistics> frameBasedStats = new SimpleObjectProperty<>();
+
+    private ObservableList<PlayerVisualsRow> playerVisualsRows = FXCollections.observableArrayList();
+    private ObservableMap<String, Node> mapElements = FXCollections.observableHashMap();
+
+    // Players whose values will always be up to date with the current frame
+    private List<Player> players;
+
+    @InjectViewModel
+    SettlersReplayViewModel viewModel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        players = viewModel.getPlayers();
+        frameBasedStats.bind(viewModel.frameBasedStatsProperty());
+
+        drawPane.setPrefWidth(DRAW_PANE_WIDTH);
+        var drawPanePrefHeight = DRAW_PANE_WIDTH
+                * ((double) viewModel.getMapRectangle().getHeight() / (double) viewModel.getMapRectangle().getWidth());
+        drawPane.setPrefHeight(drawPanePrefHeight);
+        coordinateHelper = new CoordinateHelper(viewModel.getMapRectangle(), drawPane.getPrefHeight(),
+                drawPane.getPrefWidth(), STANDARD_ICON_VECTOR);
+        initializePlayersVisuals();
+
+        playerDataTable.setItems(FXCollections.observableArrayList(players));
+        playerVisualsTable.setItems(playerVisualsRows);
+
+        turnValueLabel.textProperty().bind(viewModel.turnProperty().asString());
+        turnTimeStampValueLabel.textProperty().bind(viewModel.turnTimeStampProperty());
+        diceRollsValueLabel.textProperty().bind(viewModel.diceNumberRolledProperty().asString());
+
+        playPauseButton.setGraphic(playImageView);
+        veloSlider.valueProperty().bindBidirectional(viewModel.velocityProperty());
+        frameSlider.maxProperty().bind(viewModel.frameSliderMaxProperty());
+        frameSlider.valueProperty().bindBidirectional(viewModel.currentFrameProperty());
+        frameSlider.majorTickUnitProperty().bind(viewModel.frameSliderTickUnitProperty());
+        frameSlider.setBlockIncrement(1);
+        frameSlider.setSnapToTicks(false);
+
+        drawPane.getChildren().setAll(mapElements.values());
+    }
+
+    /**
+     * This method initializes all the underlying player-specific information which
+     * is used for proper handling across the replay view.
+     */
+    private void initializePlayersVisuals() {
+        for (Player player : viewModel.getPlayers()) {
+            var playerName = player.getName();
+            HashMap<String, Image> iconMap = viewModel.getIconsForPlayer(playerName);
+            player.initializeVisuals(viewModel.getPlayerColors().get(playerName), iconMap.get("playerRoad"),
+                    iconMap.get("playerVillage"), iconMap.get("playerCity"));
+            PlayerVisualsRow row = new PlayerVisualsRow(player.getName(),
+                    UiHelper.resizeImage(new ImageView(player.getPlayerRoad()), STANDARD_ICON_VECTOR),
+                    new ImageView(player.getPlayerVillage()), new ImageView(player.getPlayerCity()));
+            initializeEventListenersForRow(row, player);
+            playerVisualsRows.add(row);
+        }
+    }
+
+    /**
+     * This method initializes all event listeners that are necessary to show or
+     * hide any player-specifc visuals.
+     * 
+     * @param row    the visuals row the event listeners shall be added to.
+     * @param player the playerName used to set the property correctly.
+     */
+    private void initializeEventListenersForRow(PlayerVisualsRow row, Player player) {
+        row.getShowPlayerCheckBox().setOnAction(event -> {
+            var value = ((CheckBox) event.getSource()).isSelected();
+            player.showRoadProperty().set(value);
+            player.showVillagesProperty().set(value);
+            player.showCitiesProperty().set(value);
+
+            row.getShowPlayerRoadCheckBox().setSelected(value);
+            row.getShowPlayerVillagesCheckBox().setSelected(value);
+            row.getShowPlayerCitiesCheckBox().setSelected(value);
+            updateMapElements();
+        });
+
+        row.getShowPlayerRoadCheckBox().setOnAction(event -> {
+            var value = ((CheckBox) event.getSource()).isSelected();
+            player.showRoadProperty().set(value);
+            updateMapElements();
+        });
+
+        row.getShowPlayerVillagesCheckBox().setOnAction(event -> {
+            var value = ((CheckBox) event.getSource()).isSelected();
+            player.showVillagesProperty().set(value);
+            updateMapElements();
+        });
+
+        row.getShowPlayerCitiesCheckBox().setOnAction(event -> {
+            var value = ((CheckBox) event.getSource()).isSelected();
+            player.showCitiesProperty().set(value);
+            updateMapElements();
+        });
+    }
+
+    /**
+     * This method updates all visuals on the map accordingly. All initialized
+     * visuals of the underlying HashMap as well as for static objects and players
+     * are directly accessed and adjusted as necessary.
+     */
+    private void updateMapElements() {
+        // TODO: Optimize with more dynamic way - dont clear and simply put everything
+        // again
+        mapElements.clear();
+        // We do not have any player-independent map items in here currently
+        for (Player player : players) {
+
+            var playerName = player.getName();
+
+            for (int i = 0; i < player.streetPositionsProperty().get().size(); i++) {
+
+                ImageView playerStreet = new ImageView(player.getPlayerRoad());
+                UiHelper.adjustVisual(playerStreet, player.showRoadProperty().get(),
+                        coordinateHelper.translateAccordingToMap(player.streetPositionsProperty().get().get(i), true));
+                mapElements.put(playerName + "_street_" + i, playerStreet);
+            }
+
+            for (int i = 0; i < player.villagePositionsProperty().get().size(); i++) {
+
+                ImageView playerVillage = new ImageView(player.getPlayerVillage());
+                UiHelper.adjustVisual(playerVillage, player.showVillagesProperty().get(),
+                        coordinateHelper.translateAccordingToMap(player.villagePositionsProperty().get().get(i), true));
+                mapElements.put(playerName + "_village_" + i, playerVillage);
+            }
+
+            for (int i = 0; i < player.cityPositionsProperty().get().size(); i++) {
+
+                ImageView playerCity = new ImageView(player.getPlayerRoad());
+                UiHelper.adjustVisual(playerCity, player.showCitiesProperty().get(),
+                        coordinateHelper.translateAccordingToMap(player.cityPositionsProperty().get().get(i), true));
+                mapElements.put(playerName + "_city_" + i, playerCity);
+            }
+        }
+    }
+
+    @FXML
+    public void handlePlayPause(ActionEvent event) {
+        if (playPauseButton.isSelected()) {
+            playPauseButton.setGraphic(pauseImageView);
+            viewModel.playData().execute();
+        } else {
+            playPauseButton.setGraphic(playImageView);
+            viewModel.pauseData().execute();
+        }
+    }
+
 }
--- src/main/java/org/visab/newgui/visualize/settlers/viewmodel/SettlersReplayViewModel.java
+++ src/main/java/org/visab/newgui/visualize/settlers/viewmodel/SettlersReplayViewModel.java
@@ -1,7 +1,275 @@
 package org.visab.newgui.visualize.settlers.viewmodel;
 
-import org.visab.newgui.ViewModelBase;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-public class SettlersReplayViewModel extends ViewModelBase {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.visab.globalmodel.IStatistics;
+import org.visab.globalmodel.Rectangle;
+import org.visab.globalmodel.settlers.SettlersFile;
+import org.visab.globalmodel.settlers.SettlersStatistics;
+import org.visab.newgui.ResourceHelper;
+import org.visab.newgui.UiHelper;
+import org.visab.newgui.visualize.ILiveViewModel;
+import org.visab.newgui.visualize.ReplayViewModelBase;
+import org.visab.newgui.visualize.VisualizeScope;
+import org.visab.newgui.visualize.settlers.model.Player;
+import org.visab.processing.ILiveViewable;
+import org.visab.util.StreamUtil;
+
+import de.saxsys.mvvmfx.InjectScope;
+import de.saxsys.mvvmfx.utils.commands.Command;
+import javafx.application.Platform;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
+public class SettlersReplayViewModel extends ReplayViewModelBase<SettlersFile>
+        implements ILiveViewModel<SettlersStatistics> {
+
+    // Thread necessary to control data updating in the background
+    private Thread updateLoop;
+
+    @InjectScope
+    VisualizeScope scope;
+
+    // Logger needs .class for each class to use for log traces
+    private static Logger logger = LogManager.getLogger(SettlersReplayViewModel.class);
+
+    // Controls that are responsible to handle the JavaFX control element actions
+    private Command playData;
+    private Command pauseData;
+
+    // Used to control the speed in which the data is updated in the replay view
+    private double updateInterval;
+
+    // Generic properties of the replay view to display information
+    private IntegerProperty frameSliderMaxProperty = new SimpleIntegerProperty();
+    private IntegerProperty frameSliderTickUnitProperty = new SimpleIntegerProperty();
+    private IntegerProperty velocityProperty = new SimpleIntegerProperty(1);
+
+    private IntegerProperty turnProperty = new SimpleIntegerProperty();
+    private StringProperty turnTimeStampProperty = new SimpleStringProperty();
+    private IntegerProperty diceNumberRolledProperty = new SimpleIntegerProperty();
+
+    // Contains all information that changes based on the frame
+    private List<SettlersStatistics> data = new ArrayList<SettlersStatistics>();
+
+    private IntegerProperty currentFrameProperty = new SimpleIntegerProperty();
+
+    // Viewmodel always has a reference to the statistics of the current stat
+    private ObjectProperty<SettlersStatistics> frameBasedStatsProperty = new SimpleObjectProperty<>();
+    private List<Player> players = new ArrayList<>();
+
+    public void initialize() {
+
+        // Update loop eventually needs to be stopped on stage close
+        scope.registerOnStageClosing(stage -> {
+            if (updateLoop != null) {
+                updateLoop.interrupt();
+            }
+        });
+
+        // Default update interval of 0.1 seconds
+        updateInterval = 100;
+
+        if (scope.isLive()) {
+            initialize(scope.getSessionListener());
+        } else {
+            initialize(scope.getFile());
+        }
+
+        data = file.getStatistics();
+
+        // Make the frame sliders values always reasonable according to shooter file
+        frameSliderMaxProperty.set(data.size() - 1);
+
+        var tickUnit = data.size() / 10;
+        // Make sure it is at least 1,
+        tickUnit = tickUnit < 1 ? 1 : tickUnit;
+        frameSliderTickUnitProperty.set(tickUnit);
+
+        for (var name : file.getPlayerNames())
+            players.add(new Player(name));
+
+        updateCurrentGameStatsByFrame(0);
+
+        // Add listener that will update the players and the general data
+        currentFrameProperty.addListener((o, oldValue, newValue) -> {
+            updateCurrentGameStatsByFrame(newValue.intValue());
+        });
+
+    }
+
+    /**
+     * This method is responsible to update the given statistics according to the
+     * global variable of the currently selected frame based on various types of
+     * inputs or events.
+     * 
+     * The view model always refers to a specific index of the overall statistics
+     * that have been loaded from the file to properly display the relevant
+     * information on the underlying UI of the CBR Shooter visualizer.
+     * 
+     */
+    private void updateCurrentGameStatsByFrame(int frame) {
+        var statistics = data.get(frame);
+
+        // This object holds all information that is available
+        frameBasedStatsProperty.set(statistics);
+
+        turnProperty.set(statistics.getTurn());
+        turnTimeStampProperty.set(statistics.getTurnTimeStamp());
+        diceNumberRolledProperty.set(statistics.getDiceNumberRolled());
+
+        for (var player : players) {
+            var globalmodelPlayer = StreamUtil.firstOrNull(data.get(frame).getPlayers(),
+                    x -> x.getName().equals(player.getName()));
+            player.updatePlayerData(globalmodelPlayer);
+        }
+    }
+
+    public Command playData() {
+        playData = runnableCommand(() -> {
+            logger.debug("Pressed play button.");
+            // Start this as a thread to provide the possibility of interrupting it on pause
+            updateLoop = new Thread() {
+                @Override
+                public void run() {
+                    while (!this.isInterrupted()) {
+                        // Always hold the update UI information
+                        // This way is necessary, because UI changes are not allowed from another thread
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                currentFrameProperty.set(Math.min((currentFrameProperty.get() + 1), data.size() - 1));
+                            }
+                        });
+                        // Sleeping time depends on the velocity sliders value
+                        try {
+                            sleep((long) updateInterval / velocityProperty.get());
+                        } catch (InterruptedException e) {
+                            // Exception is thrown when the stop button interrupts this thread
+                            Thread.currentThread().interrupt();
+                        }
+
+                    }
+                }
+            };
+            updateLoop.start();
+        });
+        return playData;
+    }
+
+    public Command pauseData() {
+        pauseData = runnableCommand(() -> {
+            if (updateLoop != null) {
+                updateLoop.interrupt();
+            }
+        });
+        return pauseData;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public Rectangle getMapRectangle() {
+        return file.getMapRectangle();
+    }
+
+    public Image getMapImage() {
+        return new Image(ResourceHelper.IMAGE_PATH + "fps_map.png");
+        // Not in here yet
+        // return new Image(new ByteArrayInputStream(file.getImages().getMap()));
+    }
+
+    public HashMap<String, Color> getPlayerColors() {
+        HashMap<String, Color> playerColorMap = new HashMap<String, Color>();
+        for (String playerName : file.getPlayerNames()) {
+            playerColorMap.put(playerName, UiHelper.translateHexToRgbColor(file.getPlayerColors().get(playerName)));
+        }
+        return playerColorMap;
+    }
+
+    public HashMap<String, Image> getIconsForPlayer(String playerName) {
+        // TODO: Later on replace with original visuals of the map --- only placeholders
+        Color playerColor = UiHelper.translateHexToRgbColor(file.getPlayerColors().get(playerName));
+        HashMap<String, Image> iconMap = new HashMap<String, Image>();
+        iconMap.put("playerRoad",
+                UiHelper.recolorImage(new Image(ResourceHelper.IMAGE_PATH + "/weapon.png"), playerColor));
+        iconMap.put("playerVillage",
+                UiHelper.recolorImage(new Image(ResourceHelper.IMAGE_PATH + "/playerPlanChange.png"), playerColor));
+        iconMap.put("playerCity",
+                UiHelper.recolorImage(new Image(ResourceHelper.IMAGE_PATH + "/playerDeath.png"), playerColor));
+        return iconMap;
+    }
+
+    public ObjectProperty<SettlersStatistics> frameBasedStatsProperty() {
+        return frameBasedStatsProperty;
+    }
+
+    public IntegerProperty frameSliderMaxProperty() {
+        return frameSliderMaxProperty;
+    }
+
+    public IntegerProperty frameSliderTickUnitProperty() {
+        return frameSliderTickUnitProperty;
+    }
+
+    public IntegerProperty diceNumberRolledProperty() {
+        return diceNumberRolledProperty;
+    }
+
+    public IntegerProperty currentFrameProperty() {
+        return currentFrameProperty;
+    }
+
+    public IntegerProperty velocityProperty() {
+        return velocityProperty;
+    }
+
+    public IntegerProperty turnProperty() {
+        return turnProperty;
+    }
+
+    public StringProperty turnTimeStampProperty() {
+        return turnTimeStampProperty;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void initialize(ILiveViewable<? extends IStatistics> listener) {
+        var concreteListener = (ILiveViewable<SettlersStatistics>) listener;
+        concreteListener.addViewModel(this);
+
+        initialize(concreteListener.getCurrentFile());
+
+    }
+
+    private int statisticsReceived = 0;
+
+    @Override
+    public void onStatisticsAdded(SettlersStatistics newStatistics, List<SettlersStatistics> statisticsCopy) {
+        statisticsReceived += 1;
+        data.add(newStatistics);
+
+        if (statisticsReceived % 20 == 0)
+            frameSliderMaxProperty.set(data.size() - 1);
+        // TODO: If current frame is last, advance.
+
+    }
+
+    @Override
+    public void onSessionClosed() {
+        // TODO Auto-generated method stub
+
+    }
 
 }
