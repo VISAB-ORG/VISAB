0892f534487d9af4d6608c45fd050e528267a3bc
TimKuno <tim.kunold@gmx.de>
2021-08-27 09:44:12 +0200

    remove unused imports

--- src/main/java/org/visab/gui/visualize/settlers/view/SettlersStatisticsDetailView.java
+++ src/main/java/org/visab/gui/visualize/settlers/view/SettlersStatisticsDetailView.java
@@ -4,15 +4,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import org.visab.gui.visualize.settlers.viewmodel.SettlersStatisticsViewModel;
-import org.visab.workspace.Workspace;
 
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
-import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.StackedBarChart;
-import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 
